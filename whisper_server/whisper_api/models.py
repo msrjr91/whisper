5da1db90bdf98a575d0baa8bcf6c13cdeb7d8df2
Michael <michael.s.ross10@gmail.com>
2023-04-08 19:58:07 -0600

    initialized first user model with first draft of fake accounts using faker

--- whisper_server/whisper_api/models.py
+++ whisper_server/whisper_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+
+
+class User(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField(max_length=100)
+  password = models.CharField(max_length=100)
+  username = models.CharField(max_length=100)
+  
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'password', 'username']
+  
+  
