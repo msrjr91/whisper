f7ab1bbbbb217d191af86fe7694deea07d48c3e3
Michael <michael.s.ross10@gmail.com>
2023-04-16 17:00:29 -0600

    sign in and register page bootstrap

--- whisper/whisper_api/forms.py
+++ whisper/whisper_api/forms.py
@@ -1,6 +1,9 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import Post, Profile
+from django.contrib.auth.models import User
+
 
 class PostForm(ModelForm):
   class Meta:
@@ -20,4 +23,20 @@ class PostForm(ModelForm):
 class UpdateProfileForm(ModelForm):
   class Meta:
     model = Profile
-    fields = ['avatar']
+    fields = '__all__'
+
+class RegisterUserForm(UserCreationForm):
+  email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+  first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+  last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+  
+  class Meta:
+    model = User
+    fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+    
+  def __init__(self, *args, **kwargs):
+    super(RegisterUserForm, self).__init__(*args, **kwargs)
+    
+    self.fields['username'].widget.attrs['class'] = 'form-control'
+    self.fields['password1'].widget.attrs['class'] = 'form-control'
+    self.fields['password2'].widget.attrs['class'] = 'form-control'
