e8db1d26b2f45fb631e0af84226dece4ed6bd7bb
Michael <michael.s.ross10@gmail.com>
2023-04-16 23:24:37 -0600

    testing deployment

--- whisper/whisper_django/settings.py
+++ whisper/whisper_django/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = "django-insecure-l0hm@dm-2taavh$x!onl^wx2&*5+os=*zwx^eroh-4w!s1(m$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
